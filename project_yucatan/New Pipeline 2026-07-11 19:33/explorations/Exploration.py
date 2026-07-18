38b1859a8bb144ea38a33e37e137a46d632af363
Rama Marin <ramamarin2013@gmail.com>
2026-07-18 11:47:00 -0600

    Delete project_yucatan/New Pipeline 2026-07-11 19:33 directory

--- project_yucatan/New Pipeline 2026-07-11 19:33/explorations/Exploration.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-# /// script
-# [tool.databricks.environment]
-# environment_version = "5"
-# ///
-# MAGIC %sql
-# MAGIC SELECT *
-# MAGIC FROM yucatan_project.security_visualization.incidencias_municipales_silver
-# MAGIC LIMIT 5;
